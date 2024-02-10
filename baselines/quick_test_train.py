fixing up some action masking.

baselines/quick_test_train.py
@@ -10,6 +10,7 @@ from stable_baselines3 import PPO
 from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
 from stable_baselines3.common.callbacks import CheckpointCallback, CallbackList, EvalCallback
 
+from sb3_contrib.common.envs import InvalidActionEnvDiscrete
 from sb3_contrib.common.maskable.policies import MaskableActorCriticPolicy
 from sb3_contrib.common.wrappers import ActionMasker
 from sb3_contrib.ppo_mask import MaskablePPO
@@ -26,40 +27,35 @@ def mask_fn(env: gym.Env) -> np.ndarray:
 
 def make_env(rank=0, config={}, seed=0):
 
-
     def __init():
-
         env = SplendorSelfPlayEnv()
-        env = ActionMasker(env, mask_fn)
         env.reset(seed=(seed + rank))
         return env
     
     return __init
-
-
-#check_env(make_env(), True, True)
-
 
 if __name__ == '__main__':
 
     sess_id = str(uuid.uuid4())[:8]
     sess_path = Path(f'experiments/session_{sess_id}')
 
-    ep_length = 32_000_000 // 32
+    ep_length = 2048 * 10 * 4
+    cpu_multiplier = 1.0
     env_config = {}
-    num_cpu = 32
+    n_steps = int(5120 // cpu_multiplier)
+    num_cpu = int(32 * cpu_multiplier)
+    #env = make_env(0, env_config)
     env = SubprocVecEnv([make_env(i, env_config) for i in range(num_cpu)])
     checkpoint_callback = CheckpointCallback(save_freq=ep_length, save_path=sess_path,
                                      name_prefix='splendor')
     
-    eval_callback = SelfPlayCallback(eval_env=env,best_model_save_path="experiments/self_play",log_path="experiments/self_play", eval_freq=ep_length)
-
+    eval_callback = SelfPlayCallback(eval_env=env,best_model_save_path="experiments/self_play",log_path="experiments/self_play", eval_freq=ep_length, verbose=1)
 
     callbacks = [checkpoint_callback,eval_callback]
 
-    learn_steps = 1
+    learn_steps = 40
     
-    model = PPO("MultiInputPolicy", env, verbose=1, n_steps=ep_length // 8, batch_size=500_000, n_epochs=3, gamma=0.998, tensorboard_log=sess_path)
+    model = MaskablePPO("MultiInputPolicy", env, verbose=1, n_steps=n_steps, batch_size=512, n_epochs=10, gamma=0.998, tensorboard_log=sess_path, ent_coef=0.01, learning_rate=0.0003, vf_coef=0.5)
     
     for i in range(learn_steps):
         model.learn(total_timesteps=ep_length*num_cpu*1000, callback=CallbackList(callbacks))